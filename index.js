836acd2b2568b171cba49bd18be885cc80b770df
Samriddho Kar <samriddhokar8@gmail.com>
2026-05-31 17:23:41 +0530

    Added Drum Kit Project

---
+++ index.js
@@ -0,0 +1,56 @@
+var Drums = document.querySelectorAll(".drum").length;
+for(var i=0; i<Drums; i++)
+{
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        var buttonInnerHtml = this.innerHTML;
+        sounds(buttonInnerHtml);
+        buttonAnimation(buttonInnerHtml);
+       
+    });
+}    
+    document.addEventListener("keydown", function(event){
+        sounds(event.key);
+        buttonAnimation(event.key);
+
+    });
+function sounds(key){
+    switch(key){
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+        break;
+        case "s":   
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+        break;
+        case "a":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+        break;
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+        break;
+        case "j":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+        break;
+        case "k":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+        break;
+        case "l":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+        break;
+        default:
+            console.log(buttonInnerHtml);
+    }
+}
+function buttonAnimation(keyPressed){
+    var activeButton = document.querySelector("."+ keyPressed);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed")
+        }, 100);
+}
